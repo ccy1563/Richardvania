2caad2029d2a50d62423f02851c3479ce4e66ade
ccy1563 <ccy1563@gmail.com>
2021-10-05 01:37:11 -0400

    edit instruction and social

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ window.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById('canvas1');
     canvas.width = 800;
     canvas.height = 500;
-
     new Game(canvas);
 })
